f1daf9766f12972afc1cda0a42c7dee0137e3964
Ghislain H <telitos@gmail.com>
2016-12-13 21:42:49 +0000

    adds validation examples

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+
+    it "should save if all fields are filled" do
+
+      @category = Category.create({
+        name: "All"})
+
+      @product = @category.products.create({
+        name: "Iron",
+        price: 34.2,
+        quantity: 2,
+        })
+      expect(@product).to be_valid
+
+    end
+
+    it "should not save without a name" do
+      @category = Category.create({
+        name: "All"})
+
+      @product = @category.products.create({
+        name: nil,
+        price: 34.2,
+        quantity: 2,
+        })
+      expect(@product).to be_invalid
+    end
+
+    it "should not save without a price" do
+      @category = Category.create({
+        name: "All"})
+
+      @product = @category.products.create({
+        name: "JumboShrimp",
+        price: nil,
+        quantity: 2,
+        })
+      expect(@product).to be_invalid
+    end
+
+    it "should not save without a quantity" do
+      @category = Category.create({
+        name: "All"})
+
+      @product = @category.products.create({
+        name: "JumboShrimp",
+        price: nil,
+        quantity: nil,
+        })
+      expect(@product).to be_invalid
+    end
+
+    it "should not save without a category" do
+
+      @product = Product.create({
+        name: "JumboShrimp",
+        price: nil,
+        quantity: 2,
+        category: nil
+        })
+       expect(@product).to be_invalid
+    end
+  end
+end
